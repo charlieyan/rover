1efb43a6f85315b2ae32c2529cee268c78bfcee3
Charlie Yan <charlieyan08@gmail.com>
2017-05-19 23:18:41 -0700

    left and right motor working

--- hobby_gearmotor_test/hobby_gearmotor_test.ino
+++ hobby_gearmotor_test/hobby_gearmotor_test.ino
@@ -1,12 +1,17 @@
 #include <SparkFun_TB6612.h>
 
-#define PWMB 23
-#define BIN2 22
-#define BIN1 21
-#define STBY 20
+#define PWMA 23
+#define AIN2 19
+#define AIN1 18
+#define STBY 17
 
-const int offsetB = 1;
-Motor motor2 = Motor(BIN1, BIN2, PWMB, offsetB, STBY);
+#define PWMB 22
+#define BIN2 16
+#define BIN1 15
+
+const int offset = 1;
+Motor motor_l = Motor(AIN1, AIN2, PWMA, offset, STBY);
+Motor motor_r = Motor(BIN1, BIN2, PWMB, offset, STBY);
 
 String readString;
 
@@ -28,18 +33,12 @@ void loop()
   }
 
   if (readString == "start") {
-    motor2.drive(255,1000);
-    motor2.drive(-255,1000);
-    motor2.brake();
-    delay(1000);
+    motor_l.drive(255,1000);
+    motor_r.drive(255,1000);
   }
   else if (readString == "stop") {
-
+    motor_l.brake();
+    motor_r.brake();
   }
   readString = "";
-
-  motor2.drive(255,1000);
-  motor2.drive(-255,1000);
-//  motor2.brake();
-  Serial.println("drove it");
 }
