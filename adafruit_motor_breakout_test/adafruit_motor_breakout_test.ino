4fa09ad3f0ccbc748da21147aec14421dcbdeb77
Charlie Yan <charlieyan08@gmail.com>
2017-05-24 01:10:07 -0700

    adafruit breakout same library as sparkfun breakout

---
+++ adafruit_motor_breakout_test/adafruit_motor_breakout_test.ino
@@ -0,0 +1,41 @@
+#include <SparkFun_TB6612.h>
+
+#define PWMA 23
+#define AIN2 19
+#define AIN1 18
+#define STBY 17
+
+#define PWMB 22
+#define BIN2 16
+#define BIN1 15
+
+const int offset = 1;
+Motor motor_l = Motor(AIN1, AIN2, PWMA, offset, STBY);
+
+String readString;
+
+void setup()
+{
+  Serial.begin(115200);
+  Serial.println("starting");
+}
+ 
+void loop()
+{
+  while (Serial.available()) {
+    char c = Serial.read(); // gets one byte from serial buffer
+    readString += c; //makes the string readString
+    delay(2); //slow looping to allow buffer to fill with next character
+  }
+  if (readString.length() > 0) {
+    Serial.println(readString);
+  }
+
+  if (readString == "start") {
+    motor_l.drive(255,1000);
+  }
+  else if (readString == "stop") {
+    motor_l.brake();
+  }
+  readString = "";
+}
