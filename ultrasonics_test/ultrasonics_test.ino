2837f85f57777c28292625f5c6c02947f7c48c4a
Charlie Yan <charlieyan08@gmail.com>
2017-05-23 22:35:36 -0700

    ultrasonics test works

--- ultrasonics_test/ultrasonics_test.ino
+++ ultrasonics_test/ultrasonics_test.ino
@@ -2,15 +2,12 @@
 
 #include <Ultrasonic.h>
 
-Ultrasonic ultrasonic(5,6); // (Trig PIN,Echo PIN)
+Ultrasonic ultrasonic(14,13); // (Trig PIN,Echo PIN), 5V tolerant pins
 //Ultrasonic ultraright(6,7);  // (Trig PIN,Echo PIN)
 
 void setup() {
   Serial.begin(9600);
-  pinMode(4, OUTPUT); // VCC pin
-  pinMode(7, OUTPUT); // GND ping
-  digitalWrite(4, HIGH); // VCC +5V mode  
-  digitalWrite(7, LOW);  // GND mode 
+  // Make sure VCC is V_IN 5V on teensy 3.2
 }
 
 void loop()
